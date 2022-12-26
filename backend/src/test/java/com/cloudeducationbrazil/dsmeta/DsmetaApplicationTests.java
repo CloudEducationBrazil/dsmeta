6ff4dc80118e3156360533241c02f8b2094193f0
Heleno Cardoso da Silva Filho <helenocardosofilho@gmail.com>
2022-12-26 11:18:09 -0300

    iniciando o projeto web dsmeta

---
+++ backend/src/test/java/com/cloudeducationbrazil/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudeducationbrazil.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
