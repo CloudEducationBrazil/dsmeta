0cda376817f9a0ad0d837e97ad70e46d04c27df8
Heleno Cardoso da Silva Filho <helenocardosofilho@gmail.com>
2022-12-26 12:35:14 -0300

    component NotificationButton

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
 //import { useState } from 'react'
 //import './App.css'
 
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return (
+    <div>
       <h1>Vite + ReactJs + TS</h1>
+      <NotificationButton />
+    </div>
   )
 }
 
