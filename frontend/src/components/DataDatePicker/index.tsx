b3593b7a9b3ec56af344bbcc1de7176c60e4f8ea
Heleno Cardoso da Silva Filho <helenocardosofilho@gmail.com>
2022-12-26 17:20:53 -0300

    useState Component DataDatePicker

--- frontend/src/components/DataDatePicker/index.tsx
+++ frontend/src/components/DataDatePicker/index.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 function DataDatePicker() {
+    const max = new Date();
+    const [maxDate, setMaxDate] = useState(max);
+
     return (
         <DatePicker
-        selected={new Date()}
-        onChange={(date: Date) => { }}
+        selected={maxDate}
+        onChange={(date: Date) => setMaxDate(date)}
         className="dsmeta-form-control"
         dateFormat="dd/MM/yyyy"
     />
--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -4,8 +4,12 @@ import "./styles.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import DataDatePicker from "../DataDatePicker";
+import { useState } from "react";
 
 function SalesCard() {
+    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+    const [minDate, setMinDate] = useState(min);
+
     return (
         <>
             <div className="dsmeta-card">
@@ -13,14 +17,15 @@ function SalesCard() {
                 <div>
                     <div className="dsmeta-form-control-container">
                         <DatePicker
-                            selected={new Date()}
-                            onChange={(date: Date) => { }}
+                            selected={minDate}
+                            onChange={(date: Date) => setMinDate(date)}
                             className="dsmeta-form-control"
                             dateFormat="dd/MM/yyyy"
                         />
                     </div>
                     <div className="dsmeta-form-control-container">
-                        <DataDatePicker/>
+                        <DataDatePicker
+                        />
                     </div>
                 </div>
 
