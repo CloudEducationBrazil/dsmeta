899f36b34bb56805ac72ee3fd1d3b3cd587cc0ca
Heleno Cardoso da Silva Filho <helenocardosofilho@gmail.com>
2022-12-29 16:19:55 -0300

    endpoint filtro por período e send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,22 +1,37 @@
 import NotificationButton from "../NotificationButton";
 import "./styles.css";
 
-import DatePicker from "react-datepicker";
+import DatePicker, { setDefaultLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import DataDatePicker from "../DataDatePicker";
+//import DataDatePicker from "../DataDatePicker";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { BASE_URL } from "../../utils/request";
+import { Sale } from "../../models/sale";
 
 function SalesCard() {
     const min = new Date(new Date().setDate(new Date().getDate() - 365));
-    const [minDate, setMinDate] = useState(min);
+    const max = new Date();
+
+    const [dtiSale, setMinDate] = useState(min);
+    const [dtfSale, setMaxDate] = useState(max);
+
+    const [sales, setSales] = useState<Sale[]>([]);
 
     //useEffect( () => {}, []);
-    useEffect( () => {
+
+    useEffect(() => {
         //console.log('teste');
-        axios.get("http://localhost:8080/sales").then(response => {console.log(response.data);
-    })
-    }, []);
+        const dataMin = dtiSale.toISOString().slice(0,10); 
+        const dataMax = dtfSale.toISOString().slice(0,10);
+        //console.log(dataMin);
+        //console.log(dataMax);
+
+            axios.get(`${BASE_URL}/sales/periodo?dtiSale=${dataMin}&dtfSale=${dataMax}`).then(response => {
+                //console.log(response.data);
+                setSales(response.data.content);
+            });
+    }, [dtiSale, dtfSale]);
 
     return (
         <>
@@ -25,14 +40,18 @@ function SalesCard() {
                 <div>
                     <div className="dsmeta-form-control-container">
                         <DatePicker
-                            selected={minDate}
+                            selected={dtiSale}
                             onChange={(date: Date) => setMinDate(date)}
                             className="dsmeta-form-control"
                             dateFormat="dd/MM/yyyy"
                         />
                     </div>
                     <div className="dsmeta-form-control-container">
-                        <DataDatePicker
+                        <DatePicker
+                            selected={dtfSale}
+                            onChange={(date: Date) => setMaxDate(date)}
+                            className="dsmeta-form-control"
+                            dateFormat="dd/MM/yyyy"
                         />
                     </div>
                 </div>
@@ -51,6 +70,26 @@ function SalesCard() {
                             </tr>
                         </thead>
                         <tbody>
+                            {/* Dados Dinâmico*/}
+                            {sales.map(sale => {
+                                return (
+                                    <tr key={sale.id}>
+                                        <td className="show992">{sale.id}</td>
+                                        <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                        <td>{sale.sellerName}</td>
+                                        <td className="show992">{sale.visited}</td>
+                                        <td className="show992">{sale.deals}</td>
+                                        <td>{sale.amount.toFixed(2)}</td>
+                                        <td>
+                                            <div className="dsmeta-red-btn-container">
+                                                <NotificationButton saleId = {sale.id} />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                            }
+                            {/* Dados estáticos
                             <tr>
                                 <td className="show992">#341</td>
                                 <td className="show576">08/07/2022</td>
@@ -88,8 +127,8 @@ function SalesCard() {
                                     <div className="dsmeta-red-btn-container">
                                         <NotificationButton />
                                     </div>
-                                </td>
-                            </tr>
+                                </td> 
+                            </tr>*/}
                         </tbody>
                     </table>
                 </div>
