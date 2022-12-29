0e090e5edccf31941dd79c3b2033a626335e4926
Heleno Cardoso da Silva Filho <helenocardosofilho@gmail.com>
2022-12-28 21:02:00 -0300

    Twilio SMS

--- backend/src/main/java/com/cloudeducationbrazil/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/cloudeducationbrazil/dsmeta/controllers/SaleController.java
@@ -6,18 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cloudeducationbrazil.dsmeta.entities.Sale;
 import com.cloudeducationbrazil.dsmeta.services.SaleService;
+import com.cloudeducationbrazil.dsmeta.services.SmsService;
 
 @RestController
 @RequestMapping(value="/sales")
 public class SaleController {
 	@Autowired
 	private SaleService service;
+
+	@Autowired
+	private SmsService smsService;
 	
 	@GetMapping
 	public List<Sale> findAllSale(){
@@ -29,5 +34,10 @@ public class SaleController {
 									  @RequestParam(value="dtfSale", defaultValue="") String dtfSale, 
 									  Pageable pageable){
 		return service.findSalePeriodo(dtiSale, dtfSale, pageable);
+	}
+	
+	@GetMapping(value="/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
 	}
 }
